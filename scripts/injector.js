e4c38337c83d542eb089a92f2b87b6f7a84372ad
小豪 <3543601929@qq.com>
2023-05-01 20:35:21 +0800

    Create injector.js

---
+++ scripts/injector.js
@@ -0,0 +1,7 @@
+hexo.extend.injector.register('head_end', `
+
+    <meta name="baidu-site-verification" content="codeva-yHAhlAOwAB" />
+
+    <link rel="alternate" hreflang="zh-cn" href="https://blog.2to.fun"/>
+
+    `, 'default');
